fc2630f1d0fa86ddb45f6d66d2f5a39cbd915edb
xMasaki <143277398+xMasaki@users.noreply.github.com>
2025-08-19 15:30:48 -0300

    Comentando código

--- ComparacaoIdade/Program.cs
+++ ComparacaoIdade/Program.cs
@@ -6,12 +6,16 @@ namespace ComparacaoIdade
     {
         static void Main(string[] args)
         {
+            // Criando variáveis para as duas pessoas
             string nome1, nome2;
             int idade1, idade2;
 
+            // Printando texto para a pessoa 1
             Console.WriteLine("Digite o nome da primeira pessoa:");
+            // Coletando input da pessoa 1
             nome1 = Console.ReadLine();
 
+            // Mesma lógica utilizada anteriormente se repete
             Console.WriteLine("Digite a idade da primeira pessoa:");
             idade1 = int.Parse(Console.ReadLine());
 
@@ -21,10 +25,13 @@ namespace ComparacaoIdade
             Console.WriteLine("Digite a idade da segunda pessoa:");
             idade2 = int.Parse(Console.ReadLine());
 
+            // Condicional para verificar se idade da pessoa 1 é maior que a idade da pessoa 2
             if (idade1 > idade2)
             {
+                // Printando texto com concatenação para aparecer o nome caso condicional acima seja satisfeita
                 Console.WriteLine(nome1 + " é o mais velho e " + nome2 + " aluno é o mais novo.");
             }
+            // Caso primeira condição não seja satisfeita, será verificada se idade da pessoa 1 é maior que a idade da pessoa 2
             else if (idade2 > idade1)
             {
                 Console.WriteLine(nome2 + " é o mais velho e " + nome1 + " aluno é o mais novo.");
